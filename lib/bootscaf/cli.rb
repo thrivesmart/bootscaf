8007935176a9797eb9919dea6f9fe45863e1cdb7
Matthew Moore <m3moore@gmail.com>
2014-12-14 08:42:02 -0800

    WIP application.html.erb

--- lib/bootscaf/cli.rb
+++ lib/bootscaf/cli.rb
@@ -2,6 +2,7 @@ require 'thor'
 module Bootscaf
   class CLI < Thor
     YESSES = ['y', 'yes', 'Y', 'Yes', 'YES']
+    LAST_KNOWN_BOOTSTRAP_VERSION = '3.3.1'
     
     desc "version", "Ouputs the current version of Bootscaf"
     def version
@@ -18,10 +19,13 @@ module Bootscaf
       
       print "Would you like to update app/views/layouts/application.html.erb [y/n(default)]? "
       update_apphtml = $stdin.gets.strip
-      print "YOU SAID `#{update_apphtml}`"
       if YESSES.include?(update_apphtml)
-        print "Updating app/views/layouts/application.html.erb... "
-        print `sed #{inplace_command} -e 's/<title>/<title><%= yield :page_title %> /' app/views/layouts/application.html.erb`
+        latest_boostrap_version = LAST_KNOWN_BOOTSTRAP_VERSION
+        
+        print "Updating app/views/layouts/application.html.erb. "
+        print `sed #{inplace_command} -e 's/<title>/<title><%= yield :page_title %>/' app/views/layouts/application.html.erb`
+        print `sed #{inplace_command} -e 's/<%= stylesheet_link_tag    'application', media: 'all', 'data-turbolinks-track' => true %>/<link href="//maxcdn.bootstrapcdn.com/bootstrap/#{latest_boostrap_version}/css/bootstrap.min.css" rel="stylesheet">\\n<%= stylesheet_link_tag    'application', media: 'all', 'data-turbolinks-track' => true %>/' app/views/layouts/application.html.erb`
+        print "\n"
       end
     end
     
